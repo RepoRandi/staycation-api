d5c2fc27dd3172f258e67e71fb8d57521fcb33b8
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-23 11:39:21 +0700

    Create models Activity

---
+++ models/Activity.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const activitySchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+  },
+});
+
+module.exports = mongoose.model("Activity", activitySchema);
