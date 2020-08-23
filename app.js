7d5b39a2736062c7134916c4696eb05e5267356f
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-23 18:16:17 +0700

    Create connection database

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+//! import mongoose
+const mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost:27017/myapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
