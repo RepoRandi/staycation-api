ac0e34a42c4a9483b54b77984f6c203f05d24c4b
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-23 11:32:36 +0700

    Create model Feature

---
+++ models/Feature.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+
+const FeatureSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  qty: {
+    type: Number,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Feature", FeatureSchema);
