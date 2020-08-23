876d96fd396c77b173d9860cafa72a2f09fc0fa7
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-23 15:58:09 +0700

    Create view admin category

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -2,4 +2,8 @@ module.exports = {
   viewDashboard: (req, res) => {
     res.render("admin/dashboard/view_dashboard");
   },
+
+  viewCategory: (req, res) => {
+    res.render("admin/category/view_category");
+  },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -3,5 +3,6 @@ const router = require("express").Router();
 const adminController = require("../controllers/adminController");
 
 router.get("/dashboard", adminController.viewDashboard);
+router.get("/category", adminController.viewCategory);
 
 module.exports = router;
