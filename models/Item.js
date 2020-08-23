290a7491733b1e44cafba0aba2456b1ebbbffb20
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-23 11:28:30 +0700

    Create models Item

---
+++ models/Item.js
@@ -0,0 +1,49 @@
+const mongoose = require("mongoose");
+
+const { ObjectId } = mongoose.Schema;
+
+const ItemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "Indonesia",
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  imageId: [
+    {
+      type: ObjectId,
+      ref: "Image",
+    },
+  ],
+  featureId: [
+    {
+      type: ObjectId,
+      ref: "Feature",
+    },
+  ],
+  activityId: [
+    {
+      type: ObjectId,
+      ref: "Activity",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Item", ItemSchema);
