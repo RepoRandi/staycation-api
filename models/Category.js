3b687b88ddc6c9b498058089d24aea471d21ffc4
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-23 11:18:29 +0700

    Create validation

--- models/Category.js
+++ models/Category.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const categorySchema = new mongoose.Schema({
   name: {
     type: String,
+    required: true,
   },
 });
 
--- models/Image.js
+++ models/Image.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const imageSchema = new mongoose.Schema({
   imageUrl: {
     type: String,
+    required: true,
   },
 });
 
