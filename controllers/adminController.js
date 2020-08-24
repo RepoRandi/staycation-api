5d887d8a60db28e6b01d8238b22903957b10c0ee
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-24 11:27:07 +0700

    CRUD category delete

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,21 +1,19 @@
-const Category = require("../models/Category");
+const Category = require('../models/Category');
 
 module.exports = {
   viewDashboard: (req, res) => {
-    res.render("admin/dashboard/view_dashboard");
+    res.render('admin/dashboard/view_dashboard');
   },
 
   viewCategory: async (req, res) => {
     const category = await Category.find();
-    // console.log(category);
-    res.render("admin/category/view_category", { category });
+    res.render('admin/category/view_category', { category });
   },
 
   addCategory: async (req, res) => {
     const { name } = req.body;
-    // console.log(name);
     await Category.create({ name });
-    res.redirect("/admin/category");
+    res.redirect('/admin/category');
   },
 
   editCategory: async (req, res) => {
@@ -23,18 +21,25 @@ module.exports = {
     const category = await Category.findOne({ _id: id });
     category.name = name;
     await category.save();
-    res.redirect("/admin/category");
+    res.redirect('/admin/category');
+  },
+
+  deleteCategory: async (req, res) => {
+    const { id } = req.params;
+    const category = await Category.findOne({ _id: id });
+    await category.remove();
+    res.redirect('/admin/category');
   },
 
   viewBank: (req, res) => {
-    res.render("admin/bank/view_bank");
+    res.render('admin/bank/view_bank');
   },
 
   viewItem: (req, res) => {
-    res.render("admin/item/view_item");
+    res.render('admin/item/view_item');
   },
 
   viewBooking: (req, res) => {
-    res.render("admin/booking/view_booking");
+    res.render('admin/booking/view_booking');
   },
 };
--- routes/admin.js
+++ routes/admin.js
@@ -3,10 +3,11 @@ const router = require("express").Router();
 const adminController = require("../controllers/adminController");
 
 router.get("/dashboard", adminController.viewDashboard);
-
+//!  endpoint category
 router.get("/category", adminController.viewCategory);
 router.post("/category", adminController.addCategory);
 router.put("/category", adminController.editCategory);
+router.delete("/category/:id", adminController.deleteCategory);
 
 router.get("/bank", adminController.viewBank);
 router.get("/item", adminController.viewItem);
