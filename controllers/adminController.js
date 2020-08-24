7501a61f92d40faf0f750145191a9fa3f4b42547
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-24 14:25:17 +0700

    CRUD Bank-read

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -69,14 +69,16 @@ module.exports = {
     }
   },
 
-  viewBank: (req, res) => {
+  viewBank: async (req, res) => {
     try {
+      const bank = await Bank.find()
       const alertMessage = req.flash('alertMessage');
       const alertStatus = req.flash('alertStatus');
       const alert = { message: alertMessage, status: alertStatus };
       res.render('admin/bank/view_bank', {
         title: 'Staycation | Bank',
-        alert
+        alert,
+        bank
       });
     } catch (error) {
       req.flash('alertMessage', `${error.message}`);
