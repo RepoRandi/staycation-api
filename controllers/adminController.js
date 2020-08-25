2ec240f1bc1e11c4c1f47632bd685fedc2791e83
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-25 13:05:51 +0700

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -158,15 +158,18 @@ module.exports = {
 
   viewItem: async (req, res) => {
     try {
+      const item = await Item.find()
+        .populate({ path: 'imageId', select: 'id imageUrl' })
+        .populate({ path: 'categoryId', select: 'id name' });
       const category = await Category.find();
-      const bank = await Bank.find()
       const alertMessage = req.flash('alertMessage');
       const alertStatus = req.flash('alertStatus');
       const alert = { message: alertMessage, status: alertStatus };
       res.render('admin/item/view_item', {
         title: 'Staycation | Item',
         category,
-        alert
+        alert,
+        item
       });
     } catch (error) {
       req.flash('alertMessage', `${error.message}`);
