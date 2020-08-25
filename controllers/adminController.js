96ed1f78bed3f32a7f86e2a4b53a3553120d6158
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-25 11:35:56 +0700

    Update adminController.js

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,5 +1,7 @@
 const Category = require('../models/Category');
 const Bank = require('../models/Bank');
+const Item = require('../models/Item');
+const Image = require('../models/Image');
 const fs = require('fs-extra');
 const path = require('path');
 
@@ -157,10 +159,46 @@ module.exports = {
   viewItem: async (req, res) => {
     try {
       const category = await Category.find();
+      const bank = await Bank.find()
+      const alertMessage = req.flash('alertMessage');
+      const alertStatus = req.flash('alertStatus');
+      const alert = { message: alertMessage, status: alertStatus };
       res.render('admin/item/view_item', {
         title: 'Staycation | Item',
-        category
+        category,
+        alert
       });
+    } catch (error) {
+      req.flash('alertMessage', `${error.message}`);
+      req.flash('alertStatus', 'danger');
+      res.redirect('/admin/item');
+    }
+  },
+
+  addItem: async (req, res) => {
+    try {
+      const { categoryId, title, price, city, about } = req.body;
+      if (req.files.length > 0) {
+        const category = await Category.findOne({ _id: categoryId });
+        const newItem = {
+          categoryId: category._id,
+          title,
+          description: about,
+          price,
+          city
+        }
+        const item = await Item.create(newItem);
+        category.itemId.push({ _id: item._id });
+        await category.save();
+        for (let i = 0; i < req.files.length; i++) {
+          const imageSave = await Image.create({ imageUrl: `images/${req.files.name}` });
+          item.imageId.push({ _id: imageSave._id });
+          await item.save();
+        }
+        req.flash('alertMessage', 'Success add Item');
+        req.flash('alertStatus', 'success');
+        res.redirect('/admin/item');
+      }
     } catch (error) {
       req.flash('alertMessage', `${error.message}`);
       req.flash('alertStatus', 'danger');
