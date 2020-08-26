4eabafa18f70d58e2722fbeb09da97730ca267b4
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-26 10:56:58 +0700

    CRUD Activity-read

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -331,12 +331,15 @@ module.exports = {
       const alertMessage = req.flash('alertMessage');
       const alertStatus = req.flash('alertStatus');
       const alert = { message: alertMessage, status: alertStatus };
+
       const feature = await Feature.find({ itemId: itemId });
+      const activity = await Activity.find({ itemId: itemId });
       res.render('admin/item/detail_item/view_detail_item', {
         title: 'Staycation | Detail Item',
         alert,
         itemId,
-        feature
+        feature,
+        activity
       })
     } catch (error) {
       req.flash('alertMessage', `${error.message}`);
