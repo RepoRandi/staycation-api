dbfb95e49388af6ffd6866ec8c5e2502e31aefcf
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-29 17:08:08 +0700

    Create API detail-page

--- controllers/apiController.js
+++ controllers/apiController.js
@@ -2,6 +2,7 @@ const Item = require('../models/Item');
 const Treasure = require('../models/Activity');
 const Traveler = require('../models/Booking');
 const Category = require('../models/Category');
+const Bank = require('../models/Bank');
 
 module.exports = {
 
@@ -10,7 +11,7 @@ module.exports = {
       const mostPicked = await Item.find()
         .select('_id title country city price unit imageId')
         .limit(5)
-        .populate({ path: 'imageId', select: '_id imageUrl' })
+        .populate({ path: 'imageId', select: '_id imageUrl' });
 
       const category = await Category.find()
         .select('_id name')
@@ -67,6 +68,36 @@ module.exports = {
       console.log(error);
       res.status(500).json({ message: 'Internal server error' })
     }
+  },
 
-  }
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const item = await Item.findOne({ _id: id })
+        .populate({ path: 'featureId', select: '_id name qty imageUrl' })
+        .populate({ path: 'activityId', select: '_id name type imageUrl' })
+        .populate({ path: 'imageId', select: '_id imageUrl' });
+
+      const bank = await Bank.find();
+
+      testimonial = {
+        _id: "asd1293uasdads1",
+        imageUrl: "/images/testimonial2.jpg",
+        name: "Happy Family",
+        rate: 4.55,
+        content: "What a great trip with my family and I should try again next time soon ...",
+        familyName: "Randi",
+        familyOccupation: "Software Engineer"
+      }
+
+      res.status(200).json({
+        ...item._doc,
+        bank,
+        testimonial
+      })
+
+    } catch (error) {
+
+    }
+  },
 }
--- routes/api.js
+++ routes/api.js
@@ -4,5 +4,6 @@ const apiController = require('../controllers/apiController');
 // const auth = require('../middlewares/auth');
 
 router.get("/landing-page", apiController.landingPage);
+router.get("/detail-page/:id", apiController.detailPage);
 
 module.exports = router;
