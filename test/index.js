c7b1acd3a27b149c368b2fe6b54c2ac3368cb519
Randi Maulana Akbar <randimaulanaakbar@gmail.com>
2020-08-30 12:32:26 +0700

    Testing API Detail page with Mocha & Chai

--- test/index.js
+++ test/index.js
@@ -22,4 +22,33 @@ describe('API ENDPOINT TESTING', () => {
       done();
     })
   })
+
+  it('GET Detail Page', (done) => {
+    chai.request(app).get('/api/v1/member/detail-page/5e96cbe292b97300fc902222').end((err, res) => {
+      expect(err).to.be.null
+      expect(res).to.have.status(200)
+      expect(res.body).to.be.an('object')
+      expect(res.body).to.have.property('country')
+      expect(res.body).to.have.property('isPopular')
+      expect(res.body).to.have.property('unit')
+      expect(res.body).to.have.property('sumBooking')
+      expect(res.body).to.have.property('imageId')
+      expect(res.body.imageId).to.have.an('array')
+      expect(res.body).to.have.property('featureId')
+      expect(res.body.featureId).to.have.an('array')
+      expect(res.body).to.have.property('activityId')
+      expect(res.body.activityId).to.have.an('array')
+      expect(res.body).to.have.property('_id')
+      expect(res.body).to.have.property('title')
+      expect(res.body).to.have.property('price')
+      expect(res.body).to.have.property('city')
+      expect(res.body).to.have.property('description')
+      expect(res.body).to.have.property('__v')
+      expect(res.body).to.have.property('bank')
+      expect(res.body.bank).to.have.an('array')
+      expect(res.body).to.have.property('testimonial')
+      expect(res.body.testimonial).to.have.an('object')
+      done();
+    })
+  })
 })
